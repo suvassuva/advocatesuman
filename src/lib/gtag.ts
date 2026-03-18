74564696b0f2d0c22ec55afbb6bbc05c97d171e3
Vasu G <110481634+suvassuva@users.noreply.github.com>
2026-03-18 13:31:13 +0530

    Add check for window.gtag before calling

--- src/lib/gtag.ts
+++ src/lib/gtag.ts
@@ -1,7 +1,9 @@
 export const GA_ID = "G-M1C5D8KTVV";
 
 export const pageview = (url: string) => {
-  window.gtag('config', GA_ID, {
-    page_path: url,
-  });
+  if (typeof window !== "undefined" && (window as any).gtag) {
+    (window as any).gtag("config", GA_ID, {
+      page_path: url,
+    });
+  }
 };
